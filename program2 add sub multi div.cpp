f037476234f10f9fcbdf8cd3869b030cfc0d8250
SaranyaLL <112744611+SaranyaLL@users.noreply.github.com>
2022-09-07 13:34:27 +0530

    Add files via upload

---
+++ program2 add sub multi div.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"enter the of a =";
+	cin>>a;
+	cout<<"enter the of b =";,
+	cin>>b;
+	cout<<"the sum = "<<a+b<<endl;
+	cout<<"the sub ="<<a-b<<endl;
+	cout<<"the multipcation ="<<a*b<<endl;
+	cout<<"the division ="<<a/b<<endl;
+}
